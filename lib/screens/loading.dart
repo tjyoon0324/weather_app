b60f3775e0c8cc00e45ed2fbda9983ff7063af34
tj <tjyoon0324@gmail.com>
2023-01-19 22:19:17 +0900

    geolocator

--- lib/screens/loading.dart
+++ lib/screens/loading.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 
 class Loading extends StatefulWidget {
   const Loading({Key? key}) : super(key: key);
@@ -8,23 +9,30 @@ class Loading extends StatefulWidget {
 }
 
 class _LoadingState extends State<Loading> {
+
+  void getLocation() async{
+    LocationPermission permission = await Geolocator.requestPermission();
+    Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+    print(position);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body:Center(
+      body: Center(
         child: ElevatedButton(
-          onPressed: (){
-
+          onPressed: () {
+            print('Button pushed');
+            getLocation();
           },
-          child: Text('Get my location',
-          style: TextStyle(
-            color: Colors.white
-          ),
+          child: Text(
+            'Get my location',
+            style: TextStyle(color: Colors.white),
           ),
           style: ButtonStyle(
-            backgroundColor: MaterialStateProperty.all<Color>(Colors.blue)),
-          ),
+              backgroundColor: MaterialStateProperty.all<Color>(Colors.blue)),
         ),
-      );
+      ),
+    );
   }
 }
