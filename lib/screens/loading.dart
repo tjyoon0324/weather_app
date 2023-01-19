7b82fc9c6c6d5d3694193d92e5dad4f18f2a081a
tj <tjyoon0324@gmail.com>
2023-01-19 21:55:45 +0900

    Loading page created

---
+++ lib/screens/loading.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class Loading extends StatefulWidget {
+  const Loading({Key? key}) : super(key: key);
+
+  @override
+  State<Loading> createState() => _LoadingState();
+}
+
+class _LoadingState extends State<Loading> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body:Center(
+        child: ElevatedButton(
+          onPressed: (){
+
+          },
+          child: Text('Get my location',
+          style: TextStyle(
+            color: Colors.white
+          ),
+          ),
+          style: ButtonStyle(
+            backgroundColor: MaterialStateProperty.all<Color>(Colors.blue)),
+          ),
+        ),
+      )
+    );
+  }
+}
