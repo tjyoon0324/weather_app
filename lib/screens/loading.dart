d370e8f72b0dc0210e0ed5bf9509d998a62c6ca2
tj <tjyoon0324@gmail.com>
2023-01-22 21:44:05 +0900

    http package

--- lib/screens/loading.dart
+++ lib/screens/loading.dart
@@ -9,7 +9,6 @@ class Loading extends StatefulWidget {
 }
 
 class _LoadingState extends State<Loading> {
-
   @override
   void initState() {
     // TODO: implement initState
@@ -18,7 +17,7 @@ class _LoadingState extends State<Loading> {
   }
 
   void getLocation() async {
-    LocationPermission permission = await Geolocator.requestPermission();
+    // LocationPermission permission = await Geolocator.requestPermission();
     try {
       Position position = await Geolocator.getCurrentPosition(
           desiredAccuracy: LocationAccuracy.high);
@@ -27,26 +26,26 @@ class _LoadingState extends State<Loading> {
       print("Error in internet connection");
     }
   }
-}
 
-@override
-Widget build(BuildContext context) {
-  return Scaffold(
-    body: Center(
-      child: ElevatedButton(
-        onPressed: null,
-        // () {
-        // print('Button pushed');
-        // getLocation();
-        // },
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: ElevatedButton(
+          onPressed: null,
+          // () {
+          // print('Button pushed');
+          // getLocation();
+          // },
 
-        style: ButtonStyle(
-            backgroundColor: MaterialStateProperty.all<Color>(Colors.blue)),
-        child: Text(
-          'Get my location',
-          style: TextStyle(color: Colors.white),
+          style: ButtonStyle(
+              backgroundColor: MaterialStateProperty.all<Color>(Colors.blue)),
+          child: Text(
+            'Get my location',
+            style: TextStyle(color: Colors.white),
+          ),
         ),
       ),
-    ),
-  );
-}}
+    );
+  }
+}
